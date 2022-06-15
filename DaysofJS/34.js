5bf3be6f00ae4d10729e65e22238f9c1beed1753
soyhandong <21500035@handong.edu>
2022-06-15 23:51:35 +0900

    recursive, factorial

---
+++ DaysofJS/34.js
@@ -0,0 +1,83 @@
+// recursive, factorial
+/**
+Objective
+Today, we are learning about an algorithmic concept called recursion. Check out the Tutorial tab for learning materials and an instructional video.
+
+Recursive Method for Calculating Factorial
+Function Description
+Complete the factorial function in the editor below. Be sure to use recursion.
+
+factorial has the following paramter:
+
+int n: an integer
+Returns
+
+int: the factorial of 
+Note: If you fail to use recursion or fail to name your recursive function factorial or Factorial, you will get a score of .
+
+Input Format
+
+A single integer,  (the argument to pass to factorial).
+
+Constraints
+
+Your submission must contain a recursive function named factorial.
+Sample Input
+
+3
+Sample Output
+
+6
+Explanation
+
+Consider the following steps. After the recursive calls from step 1 to 3, results are accumulated from step 3 to 1.
+
+
+ */
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'factorial' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+function factorial(n) {
+    // Write your code here
+    return n<=1 ? 1 : n * factorial(n-1);
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const result = factorial(n);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
