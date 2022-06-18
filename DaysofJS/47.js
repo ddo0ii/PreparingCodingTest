5350659b497839d6bcab4b24edda9c0b6b2c11fc
soyhandong <21500035@handong.edu>
2022-06-18 23:28:03 +0900

    Binary Search Trees (BSTs)

---
+++ DaysofJS/47.js
@@ -0,0 +1,82 @@
+// Binary Search Trees (BSTs)
+/**
+Objective
+Today, we're working with Binary Search Trees (BSTs). Check out the Tutorial tab for learning materials and an instructional video!
+
+Task
+The height of a binary search tree is the number of edges between the tree's root and its furthest leaf. You are given a pointer, , pointing to the root of a binary search tree. Complete the getHeight function provided in your editor so that it returns the height of the binary search tree.
+
+Input Format
+
+The locked stub code in your editor reads the following inputs and assembles them into a binary search tree:
+The first line contains an integer, , denoting the number of nodes in the tree.
+Each of the  subsequent lines contains an integer, , denoting the value of an element that must be added to the BST.
+
+Output Format
+
+The locked stub code in your editor will print the integer returned by your getHeight function denoting the height of the BST.
+
+Sample Input
+
+7
+3
+5
+2
+1
+4
+6
+7
+Sample Output
+
+3
+Explanation
+
+The input forms the following BST:
+
+BST.png
+
+The longest root-to-leaf path is shown below:
+
+Longest RTL.png
+
+There are  nodes in this path that are connected by  edges, meaning our BST's . Thus, we print  as our answer.
+ */
+// Start of function Node
+function Node(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+}; // End of function Node
+
+// Start of function BinarySearchTree
+function BinarySearchTree() {
+    this.insert = function(root, data) {
+        if (root === null) {
+            this.root = new Node(data);
+            
+            return this.root;
+        }
+        
+        if (data <= root.data) {
+            if (root.left) {
+                this.insert(root.left, data);
+            } else {
+                root.left = new Node(data);
+            }
+        } else {
+            if (root.right) {
+                this.insert(root.right, data);
+            } else {
+                root.right = new Node(data);
+            }
+        }
+        
+        return this.root;
+    };
+    
+    // Start of function getHeight
+    this.getHeight = function(root) {
+
+        // Add your code here
+
+    }; // End of function getHeight
