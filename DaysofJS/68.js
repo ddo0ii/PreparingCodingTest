9a51ff2d863297b84ef424cbe7e4210f59700846
soyhandong <21500035@handong.edu>
2022-07-18 14:28:37 +0900

    Central Limit Theorem

--- DaysofJS/68.js
+++ DaysofJS/68.js
@@ -21,9 +21,49 @@ Output Format
 
 Print the probability that the elevator can successfully transport all  boxes, rounded to a scale of  decimal places (i.e.,  format).
 */
-
 function processData(input) {
-  //Enter your code here
+  function erf(z) {
+    const t = 1.0 / (1.0 + 0.5 * Math.abs(z));
+    const ans =
+      1 -
+      t *
+        Math.exp(
+          -z * z -
+            1.26551223 +
+            t *
+              (1.00002368 +
+                t *
+                  (0.37409196 +
+                    t *
+                      (0.09678418 +
+                        t *
+                          (-0.18628806 +
+                            t *
+                              (0.27886807 +
+                                t *
+                                  (-1.13520398 +
+                                    t *
+                                      (1.48851587 +
+                                        t *
+                                          (-0.82215223 + t * 0.17087277))))))))
+        );
+    if (z >= 0) return ans;
+    else return -ans;
+  }
+  input = input.split`\n`;
+
+  let maxWeight = input[0] * 1;
+  let boxNum = input[1] * 1;
+  let mean = input[2] * 1;
+  let sd = input[3] * 1;
+  function cumulative(mean, sd, x) {
+    let parameter = (x - mean) / (sd * Math.sqrt(2));
+    return 0.5 * (1 + erf(parameter));
+  }
+  let sampleMean = boxNum * mean;
+  let sampleSd = Math.sqrt(boxNum) * sd;
+  let result = cumulative(sampleMean, sampleSd, maxWeight);
+  console.log(result);
 }
 
 process.stdin.resume();
